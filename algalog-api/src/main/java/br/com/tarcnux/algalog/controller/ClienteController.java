4d4b755a003ce00e1ebb26d62d821e6af9f64067
Tarcisio Nunes <github@tarcnux.com.br>
2022-03-10 20:56:50 -0300

    Cliente Model

--- algalog-api/src/main/java/br/com/tarcnux/algalog/controller/ClienteController.java
+++ algalog-api/src/main/java/br/com/tarcnux/algalog/controller/ClienteController.java
@@ -1,14 +1,32 @@
 package br.com.tarcnux.algalog.controller;
 
+import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import br.com.tarcnux.algalog.domain.model.Cliente;
 
 @RestController
 public class ClienteController {	
 	
 	@GetMapping("/clientes")
-	public String listar() {
-		return "Aloha Teste do Tatá";
+	public List<Cliente> listar() {
+		
+		var tarcnux = new Cliente();
+		tarcnux.setId(1L);
+		tarcnux.setNome("Tarcísio Nunes");
+		tarcnux.setEmail("tata@tata.com.br");
+		tarcnux.setTelefone("(47) 9 9999-8888");
+		
+		var uri = new Cliente();
+		uri.setId(2L);
+		uri.setNome("Uriel Nunes");
+		uri.setEmail("uri@tata.com.br");
+		uri.setTelefone("(47) 9 7777-8888");
+		
+		return Arrays.asList(tarcnux, uri);
 	}
 
 }
