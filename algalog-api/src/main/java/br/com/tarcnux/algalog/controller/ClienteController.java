dd4ce784c1f8bea8979264df18a663f1364ebcb6
Tarcisio Nunes <github@tarcnux.com.br>
2022-03-13 09:33:35 -0300

    Usando o Spring Data JPA

--- algalog-api/src/main/java/br/com/tarcnux/algalog/controller/ClienteController.java
+++ algalog-api/src/main/java/br/com/tarcnux/algalog/controller/ClienteController.java
@@ -5,21 +5,24 @@ import java.util.List;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.tarcnux.algalog.domain.model.Cliente;
+import br.com.tarcnux.algalog.domain.repository.ClienteRepository;
 
 @RestController
 public class ClienteController {	
 	
-	@PersistenceContext
-	private EntityManager manager;
-	
+	@Autowired
+	private ClienteRepository clienteRepository;
+		
 	@GetMapping("/clientes")
 	public List<Cliente> listar() {	
-		return manager.createQuery("from Cliente", Cliente.class)
-				.getResultList();
+		return clienteRepository.findAll();
+		//return clienteRepository.findByNome("Alice");
+		//return clienteRepository.findByNomeContaining("Ali");
 	}
 
 }
