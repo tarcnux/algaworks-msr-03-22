b12c25025e8830f27b6f0ef876a137de1855b4c7
Tarcisio Nunes <github@tarcnux.com.br>
2022-03-13 08:43:52 -0300

    Usando Jakarta Persistence (JPA)

--- algalog-api/src/main/java/br/com/tarcnux/algalog/controller/ClienteController.java
+++ algalog-api/src/main/java/br/com/tarcnux/algalog/controller/ClienteController.java
@@ -1,7 +1,9 @@
 package br.com.tarcnux.algalog.controller;
 
-import java.util.Arrays;
 import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,22 +13,13 @@ import br.com.tarcnux.algalog.domain.model.Cliente;
 @RestController
 public class ClienteController {	
 	
+	@PersistenceContext
+	private EntityManager manager;
+	
 	@GetMapping("/clientes")
-	public List<Cliente> listar() {
-		
-		var tarcnux = new Cliente();
-		tarcnux.setId(1L);
-		tarcnux.setNome("Tarcísio Nunes");
-		tarcnux.setEmail("tata@tata.com.br");
-		tarcnux.setTelefone("(47) 9 9999-8888");
-		
-		var uri = new Cliente();
-		uri.setId(2L);
-		uri.setNome("Uriel Nunes");
-		uri.setEmail("uri@tata.com.br");
-		uri.setTelefone("(47) 9 7777-8888");
-		
-		return Arrays.asList(tarcnux, uri);
+	public List<Cliente> listar() {	
+		return manager.createQuery("from Cliente", Cliente.class)
+				.getResultList();
 	}
 
 }
--- algalog-api/src/main/java/br/com/tarcnux/algalog/domain/model/Cliente.java
+++ algalog-api/src/main/java/br/com/tarcnux/algalog/domain/model/Cliente.java
@@ -1,10 +1,27 @@
 package br.com.tarcnux.algalog.domain.model;
 
+import java.util.Objects;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Cliente {
 	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
+	
+	@Column
 	private String nome;
+	
+	@Column
 	private String email;
+	
+	@Column(name = "fone")
 	private String telefone;
 	
 	public Long getId() {
@@ -32,4 +49,20 @@ public class Cliente {
 		this.telefone = telefone;
 	}
 	
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+	
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Cliente other = (Cliente) obj;
+		return Objects.equals(id, other.id);
+	}		
 }
